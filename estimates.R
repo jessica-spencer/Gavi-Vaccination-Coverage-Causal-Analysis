7f6361f88d9317c7b31e04e2aaf1a1e4b65fecc0
Jess Spencer <jspence4@conncoll.edu>
2023-07-19 19:12:40 -0400

    Update estimates.R

--- estimates.R
+++ estimates.R
@@ -1,7 +1,1 @@
-#IV design
-dat1 = dat1 %>% filter(weight>0)
-
-model1.reg = lm(fullvacc~ treatment + ccode + year + malnourished_who + wasted_who + u5mr_dhs + u5mr + imr_dhs + imr, dat = dat1)
-summary(model1.reg)
-model1.reg$coefficients[2]
 
